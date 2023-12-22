421bc64b535b27976f9550a8302a367b6a74ba09
pawelfiderek <fiderek@gmail.com>
2023-12-22 13:32:02 +0100

    initial commit

---
+++ Math.Tests/Tests.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+using Math;
+
+namespace Math.Tests
+{
+    public class Tests
+    {
+        Math calc;
+        [SetUp]
+        public void Setup()
+        {
+            calc=new Math();
+        }
+
+        [TestCase(1,1,2)]
+        [TestCase(1,5,6)]
+        public void TestAdd(double a, double b, double result)
+        {
+            double res = calc.Add(a,b);
+            Assert.AreEqual(res,result);
+        }
+
+        [TestCase(1,1,57)]
+        [TestCase(1,5,-4)]
+        public void TestMinus(double a, double b, double result)
+        {
+            double res = calc.Minus(a,b);
+            Assert.AreEqual(res,result);
+        }
+
+    }
+}
